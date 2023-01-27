57276aa5b8323ffca96371494883be3da784f032
unknown <rohith.ramakrishnan@tringapps.com>
2023-01-27 15:13:33 +0530

    ass2-2

---
+++ src/main/java/coordinate/coordinate/Coordinate.java
@@ -0,0 +1,46 @@
+package coordinate.coordinate;
+
+import java.util.*;
+import java.util.logging.Logger;
+class Coordinate implements Cloneable
+{
+        private String x;
+        private String y;
+        public Coordinate(String x,String y)
+        {
+                this.x=x;
+                this.y=y;
+        }
+        public Coordinate()
+        {
+        }
+        public boolean check(String x1,String y1)
+        {
+                return x.equals(x1) && y.equals(y1);
+        }
+        public Object clone() throws CloneNotSupportedException
+        {
+                return super.clone();
+        }
+        public static void main(String [] args) throws CloneNotSupportedException
+        {
+                Scanner sin=new Scanner(System.in);
+				Logger l=Logger.getLogger("com.api.jar");
+                l.info("Enter x1 coordinate\n");
+                String x=sin.next();
+                l.info("Enter y1 coordinate\n");
+                String y=sin.next();
+                Coordinate c=new Coordinate(x,y);
+                Coordinate c1=(Coordinate)c.clone();
+                l.info("Enter x2 and y2 coordinates\n");
+                String x1=sin.next();
+                String y1=sin.next();
+                boolean res=c1.check(x1,y1);
+                if(res==true)
+                {
+                        l.info("Same\n");
+                }
+                else
+                        l.info("Not same\n");
+        }
+}
